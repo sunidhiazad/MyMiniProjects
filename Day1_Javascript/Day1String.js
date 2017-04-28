c197729b5f3c361e6729bc3a50571548bcf1800f
sunidhiazad <sunidhi.thechopras@gmail.com>
2017-04-28 12:09:53 +0200

    Removing duplicate files

--- Day1_Javascript/Day1String.js
+++
@@ -1,9 +0,0 @@
-isString('hello'); // => true
-isString(['hello']); // => false
-isString('this is a long sentence'); // => true
-isString({ a: 2 }); // => false
-
-function isString(arg){
-	var isStringFlag = (typeof arg=='string');
-	console.log(isStringFlag);
-}
